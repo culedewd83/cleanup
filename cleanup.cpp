a710aae01611ca7902901590dd32f2b1e93b7ff5
Jesse Rogers <culedewd83@gmail.com>
2022-04-26 22:58:39 -0600

    Starting on arguments

--- cleanup.cpp
+++ cleanup.cpp
@@ -1,8 +1,21 @@
-#include<iostream>
+#include <iostream>
 #include "args.hxx"
 
-int main()
+int main(int argc, char **argv)
 {
-    std::cout << "hi" << std::endl;
+    args::ArgumentParser parser("Cleanup can be used to delete files that match certain conditions such as older then X days while keeping one file per X days", "This goes after the options.");
+    args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+
+    try {
+        parser.ParseCLI(argc, argv);
+    } catch (const args::Help&) {
+        std::cout << parser;
+        return 0;
+    } catch (const args::ParseError& e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+    
     return 0;
 }
