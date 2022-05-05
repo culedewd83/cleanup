6a38c35db30f6ad748e837d9b49fe84cfe5d6ea2
Jesse Rogers <culedewd83@gmail.com>
2022-05-05 13:02:34 -0600

    Added number of days argument

--- cleanup.cpp
+++ cleanup.cpp
@@ -8,7 +8,9 @@ int main(int argc, char **argv)
     args::ArgumentParser parser("Cleanup can be used to delete files that match certain conditions such as older then X days while keeping one file per X days", "This goes after the options.");
     args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
     args::ValueFlag<std::string> directory(parser, "path", "Directory to begin cleanup.", {'d', "dir"});
+    args::ValueFlag<int> days(parser, "Days", "Number of days between keeping a matching file. This is a required arument.", {'n', "num"});
     args::Flag testRun(parser, "Test Run", "Perform a 'test run'. Outputs the files that match the condition for deletion without deleting them.", {'t', "test"});
+    args::Flag recursive(parser, "Recursive", "Recurse sub-directories", {'r', "recursive"});
 
     // Variables
     std::filesystem::path path(std::filesystem::current_path());
@@ -23,22 +25,27 @@ int main(int argc, char **argv)
         std::cerr << "Error parsing arguments." << std::endl << std::endl;
         std::cerr << parser;
         return 1;
+    } catch (const args::ValidationError& e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
     }
     
     const bool isTestRun = testRun;
+    const bool isRecursive = recursive;
 
     if (directory) {
         std::filesystem::path p(directory.Get());
         
-        // Check is path exist
+        // Check is path exists
         if (!std::filesystem::exists(p)) {
-            std::cout << "The provided directory does not exist." << std::endl; 
+            std::cerr << "The provided directory does not exist." << std::endl; 
             return 1;
         }
 
         // Check if path is a directory
         if (!std::filesystem::is_directory(p)) {
-            std::cout << "The provided path is not a directory." << std::endl; 
+            std::cerr << "The provided path is not a directory." << std::endl; 
             return 1;
         }
 
@@ -46,7 +53,14 @@ int main(int argc, char **argv)
         path = p;
     }
 
-    
+    // Make sure the number of days was provided
+    if (!days) {
+        std::cerr << "The number of days is required." << std::endl; 
+        std::cerr << parser;
+        return 1;
+    }
+
+    const int numberOfDays = days.Get();
 
     return 0;
 }
