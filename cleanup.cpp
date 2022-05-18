6e7d11abce1e62bb22db7c8dae17359bcf91ea29
Jesse Rogers <culedewd83@gmail.com>
2022-05-18 13:13:03 -0600

    Found one more variable name that I changed to c++ standard

--- cleanup.cpp
+++ cleanup.cpp
@@ -22,7 +22,7 @@ int Cleanup::parse_arguments(int argc, char **argv) {
     args::ValueFlag<int> days(parser, "Days", "Number of days between NOT deleting a matching file. This is a required arument.", {'n', "num"});
     args::ValueFlag<int> days_before(parser, "DaysBefore", "Beginning today, the number of days to ignore before deleting matching files. Default is 0.", {'b', "before"});
     args::ValueFlag<std::string> pattern(parser, "Pattern", "A regex pattern to match files for cleanup. Files not matching the pattern will be ignored. If a regex pattern is not provided, all files will be included.", {'p', "pattern"});
-    args::Flag testRun(parser, "Test Run", "Perform a 'test run'. Outputs the files that match the condition for deletion without deleting them.", {'t', "test"});
+    args::Flag test_run(parser, "Test Run", "Perform a 'test run'. Outputs the files that match the condition for deletion without deleting them.", {'t', "test"});
     args::Flag recursive(parser, "Recursive", "Recurse sub-directories", {'r', "recursive"});
 
     try {
